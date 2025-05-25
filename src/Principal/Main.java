a099923c779887c7b487931b57980d7924190d86
Memphis <kvn_araneda@hotmail.com>
2025-05-24 23:52:05 -0400

    subida de proyecto

---
+++ src/Principal/Main.java
@@ -0,0 +1,31 @@
+import Modelos.Cancion;
+import Modelos.Posdcast;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Cancion cancion1 = new Cancion();
+
+        cancion1.setTitulo("Fearless part II ");
+        cancion1.setDuracion(188);
+        cancion1.setArtista("Chris Linton");
+        cancion1.setAlbum("Lost Sky");
+        cancion1.setGenero("Electronica");
+
+        Posdcast post1 = new Posdcast();
+
+        post1.setTitulo("Game in the house");
+        post1.setDuracion(3600);
+        post1.setRelator("GOTH");
+        post1.setTema("videoJuegos");
+        
+
+
+
+
+
+
+
+
+    }
+}
